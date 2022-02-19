abad56ad737ca34712120fef20f2a5e8487ed376
p.korolev <amlerok96@gmail.com>
2022-02-19 21:33:32 +0300

    added build.gradle and FirstJUnitTest.java and TestOrTest.java

---
+++ src/test/java/junit/FirstJUnitTest.java
@@ -0,0 +1,39 @@
+package junit;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.*;
+
+public class FirstJUnitTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("This method is executed before all tests");
+    }
+
+    @BeforeEach
+    void openBrowser() {
+        System.out.println("This method is executed before each test.");
+    }
+
+    @AfterEach
+    void after() {
+        System.out.println("This method is executed after every test.");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("This method is executed after all tests.");
+    }
+
+    @Test
+    void textTest() {
+        // поиск по тексту
+        System.out.println("              This is the first test");
+    }
+
+    @Test
+    void videoTest() {
+        // поиск видео
+        System.out.println("              This is the second test");
+    }
+}
---
+++ src/test/java/junit/TestOrTest.java
@@ -0,0 +1,35 @@
+package junit;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestOrTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void successFillTest() {
+        open("/text-box");
+        $(".main-header").shouldHave(text("Text Box"));
+
+        $("#userName").setValue("Pavel");
+        $("#userEmail").setValue("pavel@inbox.ru");
+        $("#currentAddress").setValue("Some address 1");
+        $("#permanentAddress").setValue("Other adress 2");
+        $("#submit").click();
+
+
+        $("#submit").click();
+
+    }
+}
