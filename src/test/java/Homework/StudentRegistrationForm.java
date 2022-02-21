1de72f590602ec51654f3a0ff07e1cdfc6c2c981
p.korolev <amlerok96@gmail.com>
2022-02-21 10:51:59 +0300

    added build.gradle and FirstJUnitTest.java and TestOrTest.java

--- src/test/java/Homework/StudentRegistrationForm.java
+++ src/test/java/Homework/StudentRegistrationForm.java
@@ -4,7 +4,11 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.textCaseSensitive;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -30,15 +34,41 @@ public class StudentRegistrationForm {
 
         $("#genterWrapper").$(byText("Male")).click();
 
-        $("#userNumber").setValue("+74587963581");
+        $("#userNumber").setValue("1234567894");
 
-        $("#dateOfBirthInput").setValue("19 Nov 1996");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionByValue("10");
+        $(".react-datepicker__year-select").selectOptionByValue("1996");
+        $(".react-datepicker__day--007").click();
 
-        $("#subjectsContainer").setValue("Biology, Chemistry");
+        $("#subjectsInput").setValue("Biology").pressEnter();
+        $("#subjectsInput").setValue("Chemistry").pressEnter();
 
-        $("#").setValue("");
-        $("#").setValue("");
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#uploadPicture").uploadFromClasspath("1.jpg");
 
-        $("#").click();
+        $("#currentAddress").setValue("Samara");
+
+        $("#react-select-3-input").setValue("Haryana").pressEnter();
+        $("#react-select-4-input").setValue("Panipat").pressEnter();
+        $("#submit").click();
+
+
+        $("#example-modal-sizes-title-lg").shouldHave((textCaseSensitive("Thanks for submitting the form")));
+        $(".table-responsive").shouldHave(
+                textCaseSensitive("Student Name"),    textCaseSensitive("Pavel Korolev"),
+                textCaseSensitive("Student Email"),   textCaseSensitive("pavelkorolev@corp.com"),
+                textCaseSensitive("Gender"),          textCaseSensitive("Male"),
+                textCaseSensitive("Mobile"),          textCaseSensitive("1234567894"),
+                textCaseSensitive("Date of Birth"),   textCaseSensitive("07 November,1996"),
+                textCaseSensitive("Subjects"),        textCaseSensitive("Biology, Chemistry"),
+                textCaseSensitive("Hobbies"),         textCaseSensitive("Reading"),
+                textCaseSensitive("Picture"),         textCaseSensitive("1.jpg"),
+                textCaseSensitive("Address"),         textCaseSensitive("Samara"),
+                textCaseSensitive("State and City"),  textCaseSensitive("Haryana Panipat")
+        );
+
+        $("#closeLargeModal").click();
+
     }
 }
