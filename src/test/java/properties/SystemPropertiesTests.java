ba7b835ea02f76a74500c6e958116352cd833fc5
p.korolev <amlerok96@gmail.com>
2022-04-24 17:49:54 +0300

    add properties

--- src/test/java/properties/SystemPropertiesTests.java
+++ src/test/java/properties/SystemPropertiesTests.java
@@ -3,7 +3,6 @@ package properties;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-@Tag("properties")
 public class SystemPropertiesTests {
     @Test
     void someTest1() {
