fa08c29e65bb9294f6d999d065da1ae12041bad1
p.korolev <amlerok96@gmail.com>
2022-04-26 11:58:38 +0300

    fix

--- src/test/java/testbase/TestBase.java
+++ src/test/java/testbase/TestBase.java
@@ -19,7 +19,7 @@ public class TestBase {
 
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         Configuration.remote = "https://" + getWebRemoteDriverUser() + ":" + getWebRemoteDriverPassword() + "@" + System.getProperty("remote.browser.url");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
